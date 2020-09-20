93a1c3fa60fb680bc17a3010224237746612304e
mo5tar <muhammadmokhtar92@gmail.com>
2020-09-20 14:43:49 +0200

    feat: automatic build for navigation menu, marking active section related to viewport and scrolling to sections linked to nav item clicked.

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,7 +17,8 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.getElementsByTagName('section'); 
+const navBarUList = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
@@ -25,7 +26,15 @@
  * 
 */
 
+function nearTopOfViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return  rect.top < window.innerHeight && rect.bottom >= 0;
+}
 
+function getYCoord(elem) {
+    let box = elem.getBoundingClientRect();
+    return box.top + window.pageYOffset;
+}
 
 /**
  * End Helper Functions
@@ -34,14 +43,43 @@
 */
 
 // build the nav
-
+function buildNavMen(sections) {
+    const fragment = document.createDocumentFragment();
+    for (const section of sections) {
+        const listItem = document.createElement('li');
+        const anchor = document.createElement('a');
+        anchor.setAttribute("href", "#" + section.getAttribute("id"));
+        anchor.innerText = section.dataset.nav;
+        listItem.appendChild(anchor);
+        fragment.appendChild(listItem);
+    }
+    // use DcoumentFragment temporarly change to display none and block for navBarUList
+    navBarUList.appendChild(fragment);
+}
 
 // Add class 'active' to section when near top of viewport
-
+function toggleActiveSection() {
+    for (let i = 1; i <= sections.length; i++) {
+        const section = document.getElementById('section' + i);
+        if (nearTopOfViewport(section)) {
+            section.classList.add("active");
+        } else {
+            section.classList.remove("active");
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
 
+function smothScrollTo(anchor) {
+    const section = document.querySelector(anchor.getAttribute("href"));
+    window.scrollTo({top: getYCoord(section), left: 0, behavior:"smooth"});
+}
 
+function onClick(event) {
+    event.preventDefault();
+    smothScrollTo(event.target);
+}
 /**
  * End Main Functions
  * Begin Events
@@ -49,9 +87,14 @@
 */
 
 // Build menu 
-
+document.addEventListener('DOMContentLoaded', buildNavMen(sections));
 // Scroll to section on link click
-
+const anchorElements = document.querySelectorAll('a');
+for(anchorElement of anchorElements) {
+    anchorElement.addEventListener('click', onClick);
+}
 // Set sections as active
+
+document.addEventListener('scroll', toggleActiveSection, {passive: true});
 
 
